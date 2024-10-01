cba09ea8889a6f8e6c2b69632965029ae8baf2c9
Simon Braun <simon2003braun@gmail.com>
2024-10-01 16:43:58 +0200

    Anlegen der probeklausur

---
+++ src/main/java/de/dhbw/java/probeklausur/SnatChat.java
@@ -0,0 +1,10 @@
+public class SnatChat {
+	
+	public static void main(String[] args) {
+		SnatChatRoom room = new SnatChatRoom("GansGeheim");
+		
+		room.register( new SnatChatWindow(room, new Account("Bob") ) );
+		room.register( new SnatChatWindow(room, new Account("Alice") ) );
+	}
+	
+}
